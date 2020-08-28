15e2a9a7095057dcfc86e2ce3298627f580de611
yoksel <yokselzok@gmail.com>
2020-08-28 10:51:28 +0300

    Add bag section

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const form = document.querySelector('.form');
 const submitCtrl = document.querySelector('div.form__submit');
+const addBagCtrl = document.querySelector('.form__input--add-bag');
+const colorsGroup = document.querySelector('.form__group--colors');
 
-submitCtrl.addEventListener('click', () => {
-  form.submit();
+if (submitCtrl) {
+  submitCtrl.addEventListener('click', () => {
+    form.submit();
+  });
+}
+
+addBagCtrl.addEventListener('change', () => {
+  colorsGroup.disabled = !addBagCtrl.checked;
+  colorsGroup.ariaHidden = !addBagCtrl.checked;
+
+  // Need to make consistent styles between good & bad version
+  if (colorsGroup.tagName === 'DIV') {
+    colorsGroup.classList.toggle('form__group--disabled');
+  }
 })
 
